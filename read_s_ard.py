1d07d31ddc64c9cb4fc114f6248c6f6daa08fc86
chibaf <chibaf@mac.com>
2021-09-20 01:57:39 +0900

    Add files via upload

--- read_s_ard.py
+++ read_s_ard.py
@@ -6,8 +6,14 @@ ser=serial.Serial(strPort, sys.argv[2])
 
 print("connected to: " + ser.portstr)
 
+i=0
 while True:
   line = ser.readline()
   line2=line.strip().decode('utf-8')
   data = [str(val) for val in line2.split(",")]
   print(data)
+  if i==0:
+    f = open("signal_test.csv", "w")
+    f.write(str(line2))
+    f.close()
+    i=1
--- wave_generator_01.ino
+++ wave_generator_01.ino
@@ -12,25 +12,25 @@ void setup() {
   a[i]=20.0+(0.5/9)*i;
  }
   for (int i = 0; i < 10; i = i + 1) {
-  a[i+10]=20.0-(0.5/9)*i;
+  a[i+10]=20.5-(0.5/9)*i;
  }
 // signal 2
-  float swap1,swap2,swap3;
+  float swap;
   for (int i = 0; i < na; i = i + 1) {  // copy a->b
     b[i]=a[i];
  }
- for (int j=0;j<50;j=j+1){
-  swap1=b[0];
-  b[0]=b[na-1];
+ for (int j=0;j<50;j=j+1){  // shift right 50
+  swap=b[na-1];
   for (int i = 1; i < na; i = i + 1) {
-  b[i]=b[i-1];
- }
- }
+    b[na-i]=b[na-i-1] ;
+  }
+  b[0]=swap;
+  }
 }
 
 // print signals
 void loop() {
-  for (int i = 0; i < na; i = i + 1) {  // copy a->b
+  for (int i = 0; i < na; i = i + 1) { // print to serial
     Serial.print(a[i]);
     Serial.print(",");
     Serial.print(b[i]);
